9a1241754d1abb7d5cd8d54d131d3b7d3da2c611
Oleksiy Chuguyenko (@AleksGF) <chuguyenko@gmail.com>
2023-01-16 17:43:56 +0200

    feat: add burger menu

--- plants/js/index.js
+++ plants/js/index.js
@@ -2,5 +2,37 @@ console.log(`
 `);
 
 onhashchange = e => {
-  history.replaceState(null,"", e.oldURL);
+  history.replaceState(null, "", e.oldURL);
 };
+
+window.addEventListener('load', () => {
+  const burgerBtn = document.querySelector('.header__nav_burger');
+  const navMenu = document.querySelector('.header__nav_menu');
+  const closeBtn = navMenu.querySelector('.close_cross');
+  const navMenuLinks = navMenu.querySelectorAll('.header__nav_menu_item_link');
+  let isNavMenuShows = false;
+
+  const navMenuClickHandler = (e) => {
+    if (e.target === burgerBtn) return;
+
+    if (isNavMenuShows
+      && (e.target === closeBtn
+        || [...navMenuLinks].includes(e.target)
+        || !navMenu.contains(e.target))
+    ) hideNavMenu();
+  };
+
+  const showNavMenu = () => {
+    navMenu.style.right = '15px';
+    isNavMenuShows = true;
+    document.addEventListener('click', navMenuClickHandler);
+  };
+
+  const hideNavMenu = () => {
+    navMenu.style.right = '-120px';
+    isNavMenuShows = false;
+    document.removeEventListener('click', navMenuClickHandler);
+  }
+
+  burgerBtn.addEventListener('click', showNavMenu);
+});
