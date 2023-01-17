b57230d5fb88532945aa0c9d0d4e089364afaa0b
Oleksiy Chuguyenko (@AleksGF) <chuguyenko@gmail.com>
2023-01-17 16:35:10 +0200

    feat: add activization of menu item when scroll page

--- plants/js/index.js
+++ plants/js/index.js
@@ -1,11 +1,13 @@
 console.log(`
 `);
 
+// Remove anchor link
 onhashchange = e => {
   history.replaceState(null, "", e.oldURL);
 };
 
-window.addEventListener('load', () => {
+// Handle for burger nav menu actions
+const burgerMenuHandler = () => {
   const burgerBtn = document.querySelector('.header__nav_burger');
   const navMenu = document.querySelector('.header__nav_menu');
   const closeBtn = navMenu.querySelector('.close_cross');
@@ -35,4 +37,33 @@ window.addEventListener('load', () => {
   }
 
   burgerBtn.addEventListener('click', showNavMenu);
-});
+};
+
+// Change active nav menu item
+const changeActiveNavMenuItem = () => {
+  const activeNavMenuItemHandler = (e) => {
+    const navMenuItems = document.querySelectorAll('.header__nav_menu_item');
+    const sections = document.querySelectorAll('section');
+    const menuItemsCollection = [...navMenuItems].map(el => {
+      const menuItem = el;
+      const menuItemName = el.children[0].hash.slice(1);
+      const sectionCoords = [...sections].find(el => el.id === menuItemName).getBoundingClientRect();
+      return {menuItemName, menuItem, sectionCoords};
+    });
+
+    [...menuItemsCollection].forEach((el, ind, arr) => {
+      if (el.sectionCoords.top > -150 && el.sectionCoords.top < 150) {
+        if (!el.menuItem.classList.contains('active')) {
+          arr.forEach(el => el.menuItem.classList.remove('active'));
+          el.menuItem.classList.add('active');
+        }
+      }
+    });
+  };
+
+  document.addEventListener('scroll', activeNavMenuItemHandler);
+};
+
+// Add event listeners after page loaded
+window.addEventListener('load', burgerMenuHandler);
+window.addEventListener('load', changeActiveNavMenuItem);
